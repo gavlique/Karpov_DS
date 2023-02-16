98f1976718a2b0430f7576155376cb943d9354b9
gavlique <gavlique@gmail.com>
2023-02-17 00:02:17 +0300

    hw2 done

--- deep/2 - NeuralOptim.py
+++ deep/2 - NeuralOptim.py
@@ -1,5 +1,7 @@
 import torch
 from torch import nn
+from torch.utils.data import DataLoader
+from torch.optim import Optimizer
 
 def create_model():
     return nn.Sequential(
@@ -9,4 +11,25 @@ def create_model():
     )
 
 def train(model: nn.Module, data_loader: DataLoader, optimizer: Optimizer, loss_fn):
-    
+    model.train()
+    total_loss = 0
+    for x, y in data_loader:
+        optimizer.zero_grad()  # zeroing gradients
+        output = model(x)  # forward pass
+        loss = loss_fn(output, y)  # count loss
+        total_loss += loss.item()  # writing current loss
+        loss.backward()  # backward pass
+        print(round(loss.item(), 5))  # print current loss
+        optimizer.step()  # optimization step
+    return total_loss / len(data_loader)
+
+
+def evaluate(model: nn.Module, data_loader: DataLoader, loss_fn):
+    model.eval()
+    with torch.no_grad(): # we need to turn off gradients for evaluation
+        total_loss = 0
+        for x, y in data_loader:
+            output = model(x)
+            loss = loss_fn(output, y)
+            total_loss += loss.item()
+    return total_loss / len(data_loader)
