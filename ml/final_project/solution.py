8764e4fbefd783aede950d5ce2c97cc1367559b5
gavlique <gavlique@gmail.com>
2023-05-10 23:19:34 +0300

    hw 22 halfway thru

--- ml/final_project/solution.py
+++ ml/final_project/solution.py
@@ -1,5 +1,14 @@
+import pandas as pd
+from sqlalchemy import create_engine
 import os
 from catboost import CatBoostClassifier
+from schema import PostGet
+from typing import List
+from fastapi import FastAPI
+from datetime import datetime
+
+app = FastAPI()
+
 
 def get_model_path(path: str) -> str:
     # проверяем где выполняется код в лмс, или локально. Немного магии
@@ -19,3 +28,56 @@ def load_models():
     from_file.load_model(model_path)
 
     return from_file
+
+
+def batch_load_sql(query: str) -> pd.DataFrame:
+    CHUNKSIZE = 200000
+    engine = create_engine(
+        "postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
+        "postgres.lab.karpov.courses:6432/startml"
+    )
+    conn = engine.connect().execution_options(stream_results=True)
+    chunks = []
+    for chunk_dataframe in pd.read_sql(query, conn, chunksize=CHUNKSIZE):
+        chunks.append(chunk_dataframe)
+    conn.close()
+    return pd.concat(chunks, ignore_index=True)
+
+
+def load_features():
+    liked_posts = batch_load_sql("""
+        SELECT 
+            distinct post_id, user_id
+        FROM
+            public.feed_data
+        WHERE 
+            action = 'like'
+    """)
+
+    post_features = batch_load_sql("""
+        SELECT 
+            post_id,
+            topic
+        FROM
+            public.post_text_df
+    """)
+
+    user_features = batch_load_sql("""
+        SELECT 
+            user_id,
+            age, 
+            city,
+            exp_group
+        FROM
+            public.user_data
+    """)
+
+    return liked_posts, post_features, user_features
+
+@app.get("/post/recommendations/", response_model=List[PostGet])
+def recommended_posts(
+        id: int,
+        time: datetime,
+        limit: int = 10) -> List[PostGet]:
+    # На время забьем хуй
+
