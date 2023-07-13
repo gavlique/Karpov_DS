daf37113aa126cb1c48dcbe9f9d2a7a5d44c5763
gavlique <gavlique@gmail.com>
2023-07-13 10:36:50 +0300

    start hw6

--- career/hw5-2.py
+++ career/hw5-2.py
@@ -1,26 +1,16 @@
-def min_path(grid):
-    arr = [[None for i in range(len(grid[0]))] for j in range(len(grid))]
-    arr[0][0] = grid[0][0]
-    
-    def get_min(arr, i, j):
-        if arr[i][j] is not None:
-            return arr[i][j]
+import heapq
+
+def last_stone(stones):
+    while len(stones) > 1:
+        heapq._heapify_max(stones)
+        first = stones.pop(0)
+        heapq._heapify_max(stones)
+        second = stones.pop(0)
+        diff = first - second
+        if diff == 0:
+            pass
         else:
-            if i == 0:
-                arr[i][j] = get_min(arr, i, j - 1) + grid[i][j]
-                return arr[i][j]
-            elif j == 0:
-                arr[i][j] = get_min(arr, i - 1, j) + grid[i][j]
-                return arr[i][j]
-            else:
-                arr[i][j] = min(get_min(arr, i - 1, j), get_min(arr, i, j - 1)) + grid[i][j]
-                return arr[i][j]
-    
-    return get_min(arr, len(grid) - 1, len(grid[0]) - 1)
+            stones.append(diff)
+    return 0 if len(stones) == 0 else stones[0]
 
-
-
-
-# grid = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
-
-# print(min_path(grid))
+# print(last_stone([2,3,4,5,6]))
--- career/hw5-3.py
+++ career/hw5-3.py
@@ -1,14 +1,35 @@
-def last_stone(stones: list):
-    if len(stones) <= 1:
-        return stones
-    stones.sort(reverse=True)
-    last = stones[0]
-    for i in range(1, len(stones)):
-        if last > 0:
-            last -= stones[i]
-        else:
-            last += stones[i]
-    return last
+import random
 
-# arr = [10, 3, 1, 2]
-# print(last_stone(arr))
+def sort_array(nums):
+    
+    def quicksort(nums, lo, hi):
+        if lo < hi:
+            partition_resting_point = partition(nums, lo, hi)
+            quicksort(nums, lo, partition_resting_point - 1)
+            quicksort(nums, partition_resting_point + 1, hi)
+        
+    def partition(nums, lo, hi):
+        pivotIdx = random.randint(lo, hi)
+        nums[pivotIdx], nums[hi] = nums[hi], nums[pivotIdx]
+        pivot = nums[hi]
+        
+        l_idx = lo
+        r_idx = hi-1
+        while l_idx <= r_idx:
+            if nums[l_idx] <= pivot:
+                l_idx+=1
+            elif nums[r_idx] >= pivot:
+                r_idx-=1
+            else:
+                nums[l_idx], nums[r_idx] = nums[r_idx], nums[l_idx]
+                l_idx+=1
+                r_idx-=1
+        
+        nums[l_idx], nums[hi] = nums[hi], nums[l_idx]
+        return l_idx
+    
+    quicksort(nums, 0, len(nums)-1)
+    return nums
+
+def kth_largest(array, k):
+    return sort_array(array)[-k]
